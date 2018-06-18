1f7a3becafcbf10d0ef3274c474ff09b7e16db71
Kevin Naidoo <kevin.naidoo@cashrewards.com>
2018-06-18 21:34:56 +0200

    Added urls for profile, employes list and so on ...

--- tangent/urls.py
+++ tangent/urls.py
@@ -2,9 +2,14 @@
 from django.conf.urls import url
 from employees.views import DashboardView
 from employees.views import LoginView, LogoutView
+from employees.views import EmployeeListsView
+from employees.views import ProfileView
 
 urlpatterns = [
     url(r'login/', LoginView.as_view()),
     url(r'logout/', LogoutView.as_view()),
-    url(r'', DashboardView.as_view()),
+    url(r'employees/', EmployeeListsView.as_view()),
+    url(r'employees/search/', EmployeeListsView.as_view()),
+    url(r'profile/', ProfileView.as_view()),
+    url(r'', DashboardView.as_view())
 ]
