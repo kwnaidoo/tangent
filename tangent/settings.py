7002b75f904482a77623db57e4889e5b184a8b95
Kevin Naidoo <kevin.naidoo@cashrewards.com>
2018-06-18 21:34:32 +0200

    Some pep8 / pycodestyle fixes

--- tangent/settings.py
+++ tangent/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'tangent.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 STATIC_URL = '/static/'
 
 # Used by our API manager as the base URL
